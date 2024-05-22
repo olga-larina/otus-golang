e09db55b476bf10d64c79afdb051d4529405b6a1
oalarina <OALarina@alfabank.ru>
2024-05-22 17:06:07 +0300

    HW6 is completed (fix)

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -13,8 +13,7 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 		return in
 	}
 
-	// первый стейдж получает на вход исходный канал, остальные стейджи - каналы, модицифированные предыдущими стейджами
-	out := in
+	out := generator(in, done)
 
 	for _, stage := range stages {
 		out = stage(executeStage(out, done))
@@ -23,7 +22,8 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	return out
 }
 
-func executeStage(in In, done In) Out {
+// генерация канала исходных данных с учётом получения сигналов о завершении.
+func generator(in In, done In) Out {
 	out := make(Bi)
 
 	go func() {
@@ -42,6 +42,35 @@ func executeStage(in In, done In) Out {
 				case <-done:
 					return
 				}
+			}
+		}
+	}()
+
+	return out
+}
+
+// обработка каналов для стейджа.
+func executeStage(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+
+		// необходимо прочитать все значения из входящего канала
+		// иначе предыдущий stage может зависнуть на записи, ожидая места в канале
+		for v := range in {
+			// пропускаем запись прочитанных значений, если получен сигнал завершения
+			select {
+			case <-done:
+				continue
+			default:
+			}
+
+			// ожидаем возможности записи в канал или сигнала завершения
+			select {
+			case out <- v:
+			case <-done:
+				continue
 			}
 		}
 	}()
